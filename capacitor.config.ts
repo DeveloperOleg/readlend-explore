4c03df080fee1d58a0fa0bcddb18baa6f0afbe09
Olegaxaca <108974656+DeveloperOleg@users.noreply.github.com>
2025-07-04 18:54:44 +0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.lovable.d48949ca7561423e80403f107475630f',
-  appName: 'readnest-explore',
+  appName: 'readland',
   webDir: 'dist',
   server: {
     url: "https://d48949ca-7561-423e-8040-3f107475630f.lovableproject.com?forceHideBadge=true",
